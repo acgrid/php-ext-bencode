2184e8d08fe6da19579b51a0df96e4eeb3696bc5
acgrid <acgrid@gmail.com>
2016-01-22 13:09:36 +0800

    Add version display to phpinfo output

--- cg_bcode.c
+++ cg_bcode.c
@@ -117,7 +117,8 @@ PHP_MSHUTDOWN_FUNCTION(cg_bcode)
 PHP_MINFO_FUNCTION(cg_bcode)
 {
 	php_info_print_table_start();
-	php_info_print_table_header(2, "cg_bcode support", "enabled");
+	php_info_print_table_row(2, "cg_bcode support", "enabled");
+	php_info_print_table_row(2, "bencode version", PHP_CG_BCODE_VERSION);
 	php_info_print_table_end();
 
 	/* Remove comments if you have entries in php.ini
